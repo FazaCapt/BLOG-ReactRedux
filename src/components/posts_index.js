8c1726e25f55156671913a05b1e33d0b33e43647
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-01 10:41:30 +0700

    Fixed posts menjadi post

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -9,7 +9,7 @@ class PostIndex extends Component {
     }
 
     renderPosts(){
-       return  _.map(this.props.posts, posts => {
+       return  _.map(this.props.posts, post => {
             return (
                 <li className="list-group-item" key={post.id} >
                     {post.title}
