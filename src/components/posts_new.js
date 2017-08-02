2aa321fc8ffa9cf105ea08f78038ad5f7b82bfbe
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-02 09:48:56 +0700

    Validating Forms

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -37,6 +37,36 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values) {
+    // console.log(values) -> {title: 'asdf', categories: 'asdf', content: 'asdf'}
+    const errors = {};
+
+    // validate the inputs from 'values'
+    // if(!values.title || values.title.length < 3) {
+    //     errors.title = 'Enter a title that is at least 3 characters!';
+    // }
+
+    // if(!values.title.length < 3) {
+    //     errors.title = 'Title must be at least 3 characters!';
+    // }
+    if(!values.title) {
+        errors.title = 'Enter some a title!';
+    }
+    if(!values.categories) {
+        errors.categories = 'Enter some a categories!';
+    }
+    if(!values.content) {
+        errors.content = 'Enter some a content!';
+    }
+
+    // if errors is empty, the form is fine to submit
+    // if errrors has *any* properties, redux form assumes form is invalid
+    return errors;
+}
+
+
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
